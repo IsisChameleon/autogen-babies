331d06f65fa29f748a72df9ec8cebaaeefd9bc1b
IsisChameleon <12711084+IsisChameleon@users.noreply.github.com>
2023-10-19 05:33:51 +0000

    aider: Refactored the `main()` function to parse the prompt using `argparse`, connect to the database, get table definitions, add references to the prompt, prompt the user for an SQL query, parse the response, and run the SQL query.
    # Aider chat conversation:
    
    USER: Read the comments in the main() function. Generate code based on the comments. Read db.py and llm.pyu to see what functionality you have available. Only update the main function.
    
    ASSISTANT: To generate code based on the comments in the `main()` function, we need to modify the `main.py` file.
    
    Here are the steps we need to follow:
    
    1. Parse the prompt using `argparse` to get the user's input from the command line.
    2. Connect to the database using the `with` statement and create a `db_manager`.
    3. Call `db_manager.get_table_definition_for_prompt()` to get tables in prompt ready form.
    4. Create two blank calls to `llm.add_cap_ref()` that update our current prompt passed in from the command line.
    5. Call `llm.prompt` to get a `prompt_response` variable.
    6. Parse SQL response from `prompt_response` using SQL_QUERY_DELIMITER '------------'.
    7. Call `db_manager.run_sql()` with the parsed SQL.
    
    Here is the *edit block* for the `main()` function:
    
    ```python
    src/autogen_experiments/postgres_ai_agent/main.py
    <<<<<<< HEAD
    def main():
        # parse prompt using arg parse
    
        # connect to db using with statement and create a db_manager
        with PostgresDB() as db:
            db.connect_with_url(DATABASE_URL)
            db.upsert("goals", {"id": 1, "goal_description": "second goal"})
            print(db.get_table_definition_for_prompt())
            print(db.get_all("goals"))
    
        # call db_manager.get_table_definition_for_prompt() to get tables in prompt ready form
    
        # create two blank calls to llm.add_cap_ref() that update our current prompt passed in from cli
    
        # call llm.prompt to get a prompt_response variable
    
        # parse sql response from prompt_response using SQL_QUERY_DELIMITER '------------'
    
        # call db_manager.run_sql() with the parsed sql
    
    if __name__ == '__main__':
        main()
    =======
    import argparse
    from .modules.llm import add_cap_ref, prompt
    
    def main():
        # parse prompt using arg parse
        parser = argparse.ArgumentParser()
        parser.add_argument("prompt", help="The prompt for the AI.")
        args = parser.parse_args()
    
        # connect to db using with statement and create a db_manager
        with PostgresDB() as db:
            db.connect_with_url(DATABASE_URL)
    
            # call db_manager.get_table_definition_for_prompt() to get tables in prompt ready form
            table_definitions = db.get_table_definition_for_prompt()
    
            # create two blank calls to llm.add_cap_ref() that update our current prompt passed in from cli
            prompt_with_ref = add_cap_ref(args.prompt, "Here are the table definitions:", "TABLE_DEFINITIONS", table_definitions)
            prompt_with_ref = add_cap_ref(prompt_with_ref, "Please provide the SQL query.", "SQL_QUERY", "")
    
            # call llm.prompt to get a prompt_response variable
            prompt_response = prompt(prompt_with_ref)
    
            # parse sql response from prompt_response using SQL_QUERY_DELIMITER '------------'
            sql_query = prompt_response.split('------------')[1].strip()
    
            # call db_manager.run_sql() with the parsed sql
            db.run_sql(sql_query)
    
    if __name__ == '__main__':
        main()
    >>>>>>> updated
    ```
    
    This code assumes that the SQL query returned by the AI is separated from the rest of the response by '------------'. If the delimiter is different, you should replace '------------' with the correct delimiter.

--- src/autogen_experiments/postgres_ai_agent/main.py
+++ src/autogen_experiments/postgres_ai_agent/main.py
@@ -9,26 +9,34 @@ assert os.environ.get('OPENAI_API_KEY'), 'OPENAI_API_KEY not found in .env file'
 DATABASE_URL = os.environ.get('DATABASE_URL')
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 
+import argparse
+from .modules.llm import add_cap_ref, prompt
+
 def main():
     # parse prompt using arg parse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("prompt", help="The prompt for the AI.")
+    args = parser.parse_args()
 
     # connect to db using with statement and create a db_manager
     with PostgresDB() as db:
         db.connect_with_url(DATABASE_URL)
-        db.upsert("goals", {"id": 1, "goal_description": "second goal"})
-        print(db.get_table_definition_for_prompt())
-        print(db.get_all("goals"))
 
+        # call db_manager.get_table_definition_for_prompt() to get tables in prompt ready form
+        table_definitions = db.get_table_definition_for_prompt()
 
-    # call db_manager.get_table_definition_for_prompt() to get tables in prompt ready form
+        # create two blank calls to llm.add_cap_ref() that update our current prompt passed in from cli
+        prompt_with_ref = add_cap_ref(args.prompt, "Here are the table definitions:", "TABLE_DEFINITIONS", table_definitions)
+        prompt_with_ref = add_cap_ref(prompt_with_ref, "Please provide the SQL query.", "SQL_QUERY", "")
 
-    # create two blank calls to llm.add_cap_ref() that update our current prompt passed in from cli
+        # call llm.prompt to get a prompt_response variable
+        prompt_response = prompt(prompt_with_ref)
 
-    # call llm.prompt to get a prompt_response variable
+        # parse sql response from prompt_response using SQL_QUERY_DELIMITER '------------'
+        sql_query = prompt_response.split('------------')[1].strip()
 
-    # parse sql response from prompt_response using SQL_QUERY_DELIMITER '------------'
-
-    # call db_manager.run_sql() with the parsed sql
+        # call db_manager.run_sql() with the parsed sql
+        db.run_sql(sql_query)
 
 if __name__ == '__main__':
     main()
